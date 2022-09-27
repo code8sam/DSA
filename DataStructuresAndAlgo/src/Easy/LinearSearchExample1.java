5e5f8cde6fd433382f514d1f84eff1aafbd3ee0b
code8sam <work.social.satyam@gmail.com>
2022-09-27 21:24:44 +0530

    first commit

---
+++ DataStructuresAndAlgo/src/Easy/LinearSearchExample1.java
@@ -0,0 +1,26 @@
+package Easy;
+
+public class LinearSearchExample1 {
+	public static void main(String[]args) {
+		
+		int [] nums = {23, 45, 1, 2, 8, -3};
+		int target = 8;
+		int ans = linearSearch(nums, target);
+		System.out.println(ans);
+	}
+
+	private static int linearSearch(int[] arr, int target) {
+		if(arr.length==0) {
+			return -1;
+		}
+		
+		for(int index = 0; index < arr.length; index++) {
+			int element = arr[index];
+			if(element==target) {
+				return index;
+			}
+		}
+		return -1;
+	}
+
+}
