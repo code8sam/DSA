7a3b48df17d9e71f58a445b3a2bf09d5b598b55a
code8sam <work.social.satyam@gmail.com>
2022-09-28 02:52:36 +0530

    Linear Search Code

---
+++ DataStructuresAndAlgo/src/Easy/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,35 @@
+package Easy;
+
+public class FindNumbersWithEvenNumberOfDigits {
+	
+	public static void main(String [] args) {
+		int[] nums = {12, 345, 2, 6, 7896};
+		int evenNumberOfDigits = findNumbers(nums);
+		System.out.println(evenNumberOfDigits);
+	}
+
+	private static int findNumbers(int[] nums) {
+		int count = 0;
+		for(int i=0; i<nums.length; i++) {
+			if(evenDigits(nums[i])) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	private static boolean evenDigits(int i) {
+		
+		int count =0;
+		while(i>0) {
+			count++;
+			i=i/10;
+		}
+		
+		if(count%2 == 0) {
+			return true;
+		}
+		return false;
+	}
+
+}
