828bc8a655655bbcb5388f7e8a05f2a1572b5ea9
code8sam <work.social.satyam@gmail.com>
2022-09-28 14:44:26 +0530

    Richest Customer Wealth

---
+++ DataStructuresAndAlgo/src/Easy/RichestCustomerWealth.java
@@ -0,0 +1,49 @@
+package Easy;
+
+// https://leetcode.com/problems/richest-customer-wealth/
+
+public class RichestCustomerWealth {
+	
+	public static void main(String[]args) {
+		
+		int[][] arr = {{1, 2, 3}, {3, 2, 1}, {5, 1, 1}};
+		int maximum = maximumWealth(arr);
+		System.out.println(maximum);
+		
+	}
+
+	private static int maximumWealth(int[][] accounts) {
+		
+		int[] arrayCol = new int [accounts.length];
+		
+		for(int i=0; i<accounts.length; i++) {
+			arrayCol[i] = checkSum(accounts[i]);
+		}
+		
+		int maxWealth = max(arrayCol);
+		return maxWealth;
+	}
+
+	private static int max(int[] arrayCol) {
+		
+		int max = arrayCol[0];
+		
+		for(int i=0; i<arrayCol.length; i++) {
+			if(arrayCol[i]>=max) {
+				max = arrayCol[i];
+			}
+		}
+		return max;
+	}
+
+	private static int checkSum(int[] is) {
+		
+		int sum = 0;
+		
+		for(int i=0; i<is.length; i++) {
+			sum+=is[i];
+		}
+		return sum;
+	}
+
+}
